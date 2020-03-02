daab72105c417f40c6b615cffdc804d8e3174fad
Eason Liu <onpica17@gmail.com>
2020-03-02 17:20:30 +0800

    使用 bootstrap4 建立帳號密碼輸入欄

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import React, { useState } from 'react'
-import Text from './component/Text'
-import TextInput from './component/TextInput'
+import React from 'react'
 
-function App() {
-  const [nameFromChild, setNameFromChild] = useState('')
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: '',
+    }
+  }
 
-  return (
-    <>
-      {/* sendText屬性是一個函式 */}
-      <TextInput
-        sendText={text => {
-          //傳入text，然後回設定父母元件的state值
-          setNameFromChild(text)
-        }}
-      />
-      <br />
-      <Text name={nameFromChild} />
-    </>
-  )
+  // 利用物件計算得來的屬性名稱，讓所有的輸入框用共一個方法
+  handleInput = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  render() {
+    return (
+      <>
+        <div className="container">
+          <form>
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1">帳號</label>
+              <input
+                type="text"
+                name="username"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="請輸入帳號"
+                onChange={this.handleInput}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputPassword1">密碼</label>
+              <input
+                type="password"
+                name="password"
+                className="form-control"
+                id="exampleInputPassword1"
+                placeholder="請輸入密碼"
+                onChange={this.handleInput}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              送出
+            </button>
+            {/* style屬性要注意，值必需是一個物件值*/}
+            <span style={{ color: 'red', fontSize: 20 }}>注意事項：xxxx</span>
+          </form>
+        </div>
+      </>
+    )
+  }
 }
 
 export default App
